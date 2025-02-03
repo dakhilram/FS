96c4c0a95bdb6246fc2ae85ea1528d253f5ef74f
Gargeya Akhilram Durgempudi <dakhilram@gmail.com>
2025-02-03 14:48:28 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base:"/FS/",
-})
+  base: "/FS/",  // Ensure this matches your repo name EXACTLY (case-sensitive)!
+});
