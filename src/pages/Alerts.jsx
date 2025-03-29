fad18921809a3a1657491b6eb8bdebc1b3830060
Gargeya Akhilram Durgempudi <dakhilram@gmail.com>
2025-03-29 17:08:25 -0500

    updated the alerts map pointer

--- src/pages/Alerts.jsx
+++ src/pages/Alerts.jsx
@@ -13,6 +13,20 @@ import {
   FaSyncAlt,
   FaBell,
 } from "react-icons/fa";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet's default icon paths
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 
 const API_BASE_URL = window.location.hostname === "localhost"
   ? "http://localhost:5000"
