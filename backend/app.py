a4bd63c740b7a269bbafb660795879878eb96831
Gargeya Akhilram Durgempudi <dakhilram@gmail.com>
2025-04-16 23:04:42 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1145,13 +1145,13 @@ scheduler = BackgroundScheduler(timezone=central)
 scheduler.add_job(send_daily_alert_emails, "cron", hour=0, minute=0)
 # Wildfire alerts at 8 AM and 6 PM
 scheduler.add_job(send_wildfire_risk_alerts, "cron", hour=8, minute=0)
-scheduler.add_job(send_wildfire_risk_alerts, "cron", hour=23, minute=10)
+scheduler.add_job(send_wildfire_risk_alerts, "cron", hour=23, minute=20)
 # Tornado alerts at 9 AM and 8 PM
 scheduler.add_job(send_tornado_risk_alerts, "cron", hour=9, minute=0)
-scheduler.add_job(send_tornado_risk_alerts, "cron", hour=23, minute=5)
+scheduler.add_job(send_tornado_risk_alerts, "cron", hour=23, minute=15)
 # Hurricane alerts at 10 AM and 7 PM
 scheduler.add_job(send_hurricane_risk_alerts, "cron", hour=10, minute=0)
-scheduler.add_job(send_hurricane_risk_alerts, "cron", hour=23, minute=0)
+scheduler.add_job(send_hurricane_risk_alerts, "cron", hour=23, minute=30)
 
 
 #scheduler.start()
