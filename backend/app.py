0ca50cc501dee854a9ca3c7148e3e53625ca553c
Gargeya Akhilram Durgempudi <dakhilram@gmail.com>
2025-04-16 20:08:30 -0500

    updated all the pred models to work on 9pm.

--- backend/app.py
+++ backend/app.py
@@ -1102,13 +1102,13 @@ scheduler = BackgroundScheduler(timezone=central)
 scheduler.add_job(send_daily_alert_emails, "cron", hour=0, minute=0)
 # Wildfire alerts at 8 AM and 6 PM
 scheduler.add_job(send_wildfire_risk_alerts, "cron", hour=8, minute=0)
-scheduler.add_job(send_wildfire_risk_alerts, "cron", hour=18, minute=0)
+scheduler.add_job(send_wildfire_risk_alerts, "cron", hour=21, minute=0)
 # Tornado alerts at 9 AM and 8 PM
 scheduler.add_job(send_tornado_risk_alerts, "cron", hour=9, minute=0)
-scheduler.add_job(send_tornado_risk_alerts, "cron", hour=20, minute=0)
+scheduler.add_job(send_tornado_risk_alerts, "cron", hour=21, minute=0)
 # Hurricane alerts at 10 AM and 7 PM
 scheduler.add_job(send_hurricane_risk_alerts, "cron", hour=10, minute=0)
-scheduler.add_job(send_hurricane_risk_alerts, "cron", hour=19, minute=0)
+scheduler.add_job(send_hurricane_risk_alerts, "cron", hour=21, minute=0)
 
 
 #scheduler.start()
